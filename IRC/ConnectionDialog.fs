1223ac79a2d450046ed06e379957e593719539b3
Rapmaster2000 <externc@aol.com>
2015-03-15 12:03:53 +0100

    GroupBox eingebaut, cleanups

--- IRC/ConnectionDialog.fs
+++ IRC/ConnectionDialog.fs
@@ -14,7 +14,7 @@ type ConnectionDialog =
     val mutable private tableLayoutPanel:TableLayoutPanel
     val mutable private okButton:Button 
     val mutable private cancelButton:Button
-    val mutable private adressField:TextBox
+    val mutable private addressField:TextBox
     val mutable private portField:TextBox
     [<DefaultValue>]
     val mutable private serverAdress:String
@@ -27,11 +27,11 @@ type ConnectionDialog =
 
     member public this.getCon = this.connection
 
-    member this.buttonClick  (sender:obj) (args:EventArgs) = 
+    member private this.buttonClick  (sender:obj) (args:EventArgs) = 
         try
             let s = sender :?> Button in
             if s = this.okButton then
-             this.serverAdress <- this.adressField.Text
+             this.serverAdress <- this.addressField.Text
              this.port <- Int32.Parse(this.portField.Text)
              let con = new IRCOp ("slash_Fsharp", this.serverAdress, this.port) in 
              this.connection <- con
@@ -43,45 +43,41 @@ type ConnectionDialog =
         | :? Exception as ex ->  MessageBox.Show("Fehler: " + ex.Message, "Fehler") |> ignore;
 
     member private this.init () = 
-        this.Height <- 100
-        this.Width <- 300
+        let groupBox = new GroupBox (Text = "Server-Adresse", AutoSize = true) in
+        let groupBox2 = new GroupBox(Text = "Port", AutoSize = true) in
+        //this.Height <- 100
+        //this.Width <- 300
+        this.AutoSize <- true
         this.StartPosition <- FormStartPosition.CenterScreen
         this.Text <- "Server eingeben"
         this.TopMost <- true
+        //this.FormBorderStyle <- FormBorderStyle.FixedSingle
         //this.tableLayoutPanel.Height <- 100
         //this.tableLayoutPanel.Width <- 300
+        this.tableLayoutPanel.Dock <- DockStyle.Fill
         this.tableLayoutPanel.AutoSize <- true
         let panel = new Panel () in
         let panel2 = new Panel () in
+        this.addressField.Location <- new Point(10, 20)
+        groupBox.Controls.Add  this.addressField
 
-        panel.BorderStyle <- System.Windows.Forms.BorderStyle.FixedSingle
-        panel.Controls.Add this.adressField
-        panel.Text <- "Server-Addresse"
+        this.portField.Location <- new Point (10, 20)
+        groupBox2.Controls.Add this.portField
+
+        //panel.BorderStyle <- System.Windows.Forms.BorderStyle.FixedSingle
+       // panel.Controls.Add groupBox
+        //panel.Text <- "Server-Adresse"
         panel.AutoSize <- true
+        //panel.Controls.Add groupBox
         this.okButton.AutoSize <- true
-
-        panel2.BorderStyle <- System.Windows.Forms.BorderStyle.FixedSingle
-        panel2.Controls.Add this.portField
-        panel2.Text <- "Port"
-        panel2.AutoSize <- true
-
-        this.cancelButton.Text <- "Abbrechen"
-        this.okButton.Text <- "Verbinden"
-        this.cancelButton.AutoSize <- true
-        this.tableLayoutPanel.AutoSize <- true
-        //this.cancelButton.Click.AddHandler (fun (sender:obj) (args:EventArgs) -> this.Dispose ())
+        this.tableLayoutPanel.Controls.Add (groupBox, 0 ,0) 
+        this.tableLayoutPanel.Controls.Add (groupBox2, 1 ,0) 
+        this.tableLayoutPanel.Controls.Add (this.okButton, 0, 1)
+        this.tableLayoutPanel.Controls.Add (this.cancelButton, 1, 1)
         let eventHandler  = (new EventHandler(this.buttonClick)) in
         this.cancelButton.Click.AddHandler eventHandler
         this.okButton.Click.AddHandler eventHandler
-
-
-        this.tableLayoutPanel.RowCount <- 2
-        this.tableLayoutPanel.ColumnCount <- 2
-
-        this.tableLayoutPanel.Controls.Add (panel, 0 ,0) 
-        this.tableLayoutPanel.Controls.Add (panel2, 1, 0)
-        this.tableLayoutPanel.Controls.Add (this.okButton, 0, 1)
-        this.tableLayoutPanel.Controls.Add (this.cancelButton, 1, 1)
+        (*panel2.BorderStyle <- System.Windows.Forms.BorderStyle.FixedSingle *)
         this.Controls.Add this.tableLayoutPanel
 
     static member public show () = 
@@ -91,7 +87,7 @@ type ConnectionDialog =
         ConnectionDialog.instance.Show ()
 
     new () as this = 
-       {tableLayoutPanel = new TableLayoutPanel (); okButton = new Button (); cancelButton = new Button (); adressField = new TextBox (); portField = new TextBox ()}
+       {tableLayoutPanel = new TableLayoutPanel (); okButton = new Button (Text = "Verbinden" ); cancelButton = new Button (Text="Abbrechen"); addressField = new TextBox (); portField = new TextBox ()}
        then
        this.init ()
     end
